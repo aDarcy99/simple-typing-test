02febea7cc67b2479eba6bef0cc56ca6afa008e7
Lachlan DArcygit config --global user.email darcylachlan@hotmail.com.augit add index.htmlgit statusgit config --global user.name Lachlan <darcylachlan@hotmail.com.au>
2021-07-17 12:37:45 +1000

    attempt to fix deployment error

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const IndexRouter = require("./routes/IndexRouter");
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../src")));
 app.use(express.static(path.join(__dirname, "../dist")));
-
+app.use(express.static(path.join(__dirname, "../views")));
 //routes
 app.use("/", IndexRouter)
 
